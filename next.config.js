8b0c11bfcec24248935f54507cb520741d94910c
thisishowislab <thisishowislab@gmail.com>
2025-12-03 15:20:29 -0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+```javascript
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.ctfassets.net', 'downloads.ctfassets.net'],
+  },
+}
+
+module.exports = nextConfig
+```
